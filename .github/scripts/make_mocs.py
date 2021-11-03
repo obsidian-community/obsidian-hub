0ff1d2b58303dce089d61a9aa39fca77ce5956b1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 15:14:07 +0000

    Create bullet list of file-names

--- .github/scripts/make_mocs.py
+++ .github/scripts/make_mocs.py
@@ -1,2 +1,9 @@
+import os.path
+
+
 def make_moc(directory, files):
-    pass
+    output = ''
+    for file in files:
+        name = os.path.splitext(file)[0]
+        output += f'- {name}\n'
+    return output
