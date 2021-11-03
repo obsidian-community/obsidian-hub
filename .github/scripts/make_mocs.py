e4b58660de5a06add780b7b4aec3b445d5f830da
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 15:31:55 +0000

    Extract method make_line_for_file()

--- .github/scripts/make_mocs.py
+++ .github/scripts/make_mocs.py
@@ -4,8 +4,12 @@ import os.path
 def make_moc_for_directory(directory, files):
     output = ''
     for file in files:
-        link_name, extension = os.path.splitext(file)
-        if extension != '.md':
-            link_name += extension
-        output += f'- [[{directory}/{link_name}|{link_name}]]\n'
+        output += make_line_for_file(directory, file)
     return output
+
+
+def make_line_for_file(directory, file):
+    link_name, extension = os.path.splitext(file)
+    if extension != '.md':
+        link_name += extension
+    return f'- [[{directory}/{link_name}|{link_name}]]\n'
