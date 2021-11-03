8fb22f5f8195bab3bd2ec4420cc4f9b48c703920
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 17:05:23 +0000

    Prepare for trying to move test files to separate dir

--- .github/scripts/test_make_mocs.py
+++ .github/scripts/test_make_mocs.py
@@ -6,7 +6,7 @@ from approvaltests import Options
 from approvaltests.approvals import verify
 from approvaltests.reporters import GenericDiffReporterFactory
 
-from make_mocs import make_moc_for_directory, make_moc_for_sub_directories
+import make_mocs
 
 
 def make_default_reporter():
@@ -16,12 +16,12 @@ def make_default_reporter():
 def test_output_for_files():
     directory = '01 - Topic 1/01.02 Subtopic'
     files = ['README.md', 'publish.css', 'logo.svg', 'File 1.md', 'File 2.md']
-    result = make_moc_for_directory(directory, files)
+    result = make_mocs.make_moc_for_directory(directory, files)
     verify(result)
 
 
 def test_output_for_sub_directories():
     directory = '01 - Community'
     sub_directories = ['Authors - Persons', 'Events', 'Obsidian Roundup', 'Video Channels']
-    result = make_moc_for_sub_directories(directory, sub_directories)
+    result = make_mocs.make_moc_for_sub_directories(directory, sub_directories)
     verify(result, options=(make_default_reporter()))
