d64978a1489db978e93e0de5fc800b2dc09357e7
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 15:35:51 +0000

    Rename test, in preparation for adding more

--- .github/scripts/test_make_mocs.py
+++ .github/scripts/test_make_mocs.py
@@ -5,7 +5,7 @@ from approvaltests.approvals import verify
 from make_mocs import make_moc_for_directory
 
 
-def test_simple_output():
+def test_output_for_files():
     directory = '01 - Topic 1/01.02 Subtopic'
     files = ['README.md', 'publish.css', 'logo.svg', 'File 1.md', 'File 2.md']
     result = make_moc_for_directory(directory, files)
