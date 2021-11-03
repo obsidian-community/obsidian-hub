1be3c891c84b300fd671078e9706882a4e732268
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 16:23:24 +0000

    Use hard-coded Araxis Merge for diffs - as Beyond Compare displays Unicode wrong

--- .github/scripts/test_make_mocs.py
+++ .github/scripts/test_make_mocs.py
@@ -1,8 +1,13 @@
 # TODO Figure out how to move this to a tests directory, abd still import make_moc
-
+from approvaltests import Options
 from approvaltests.approvals import verify
+from approvaltests.reporters import GenericDiffReporterFactory
 
 from make_mocs import make_moc_for_directory, make_moc_for_sub_directories
+
+
+def make_default_reporter():
+    return Options().with_reporter(GenericDiffReporterFactory().get("AraxisMergeMac"))
 
 
 def test_output_for_files():
@@ -16,4 +21,4 @@ def test_output_for_sub_directories():
     directory = '01 - Community'
     sub_directories = ['Authors - Persons', 'Events', 'Obsidian Roundup', 'Video Channels']
     result = make_moc_for_sub_directories(directory, sub_directories)
-    verify(result)
+    verify(result, options=(make_default_reporter()))
