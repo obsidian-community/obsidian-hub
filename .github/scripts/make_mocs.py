d11e98802d39ed2dd4992d592239761c5c402c27
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 16:06:38 +0000

    Start adding code to link to sub-directories

--- .github/scripts/make_mocs.py
+++ .github/scripts/make_mocs.py
@@ -13,3 +13,14 @@ def make_line_for_file(directory, file):
     if extension != '.md':
         link_name += extension
     return f'-  [[{directory}/{link_name}|{link_name}]]\n'
+
+
+def make_moc_for_sub_directories(directory, sub_directories):
+    output = ''
+    for sub_directory in sub_directories:
+        output += make_line_for_sub_directory(directory, sub_directory)
+    return output
+
+
+def make_line_for_sub_directory(directory, sub_directory):
+    return f'-  [[{sub_directory}]]\n'
--- .github/scripts/test_make_mocs.py
+++ .github/scripts/test_make_mocs.py
@@ -2,11 +2,18 @@
 
 from approvaltests.approvals import verify
 
-from make_mocs import make_moc_for_directory
+from make_mocs import make_moc_for_directory, make_moc_for_sub_directories
 
 
 def test_output_for_files():
     directory = '01 - Topic 1/01.02 Subtopic'
     files = ['README.md', 'publish.css', 'logo.svg', 'File 1.md', 'File 2.md']
     result = make_moc_for_directory(directory, files)
+    verify(result)
+
+
+def test_output_for_sub_directories():
+    directory = '01 - Community'
+    sub_directories = ['Authors - Persons', 'Events', 'Obsidian Roundup', 'Video Channels']
+    result = make_moc_for_sub_directories(directory, sub_directories)
     verify(result)
