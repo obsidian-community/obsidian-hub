02a646f9d0052f6937ae206320cca6d34bee46c8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-03 15:29:14 +0000

    Refactor to simplify code

--- .github/scripts/make_mocs.py
+++ .github/scripts/make_mocs.py
@@ -4,10 +4,8 @@ import os.path
 def make_moc(directory, files):
     output = ''
     for file in files:
-        name, extension = os.path.splitext(file)
-        if extension == '.md':
-            link_name = name
-        else:
-            link_name = file # include extension
+        link_name, extension = os.path.splitext(file)
+        if extension != '.md':
+            link_name += extension
         output += f'- [[{directory}/{link_name}|{link_name}]]\n'
     return output
