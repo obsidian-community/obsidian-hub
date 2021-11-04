0f5600c419b0c65921345c0170f3311f2f056688
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-04 18:05:47 +0000

    Comment out unused argument

--- .github/scripts/update-mocs.py
+++ .github/scripts/update-mocs.py
@@ -39,7 +39,7 @@ def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser(
         description="Create and update MOCs, mimicking Zoottelkeeper output"
     )
-    parser.add_argument("-v", "--verbose", action="store_true", default=False)
+    # parser.add_argument("-v", "--verbose", action="store_true", default=False)
 
     args = parser.parse_args(argv)
 
