69b30e256bdf2aaf0e6f388026185782030b9ab3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-04 19:25:37 +0000

    Fix typo in variable name

--- .github/scripts/make_mocs.py
+++ .github/scripts/make_mocs.py
@@ -28,11 +28,11 @@ def strip_parent_directories_from_directory(directory):
 
 
 def make_link_line(directory, link_name):
-    adjusted_direcory = strip_parent_directories_from_directory(directory)
-    if len(adjusted_direcory) > 0:
-        adjusted_direcory += '/'
-    result = f'-  [[{adjusted_direcory}{link_name}|{link_name}]]\n'
-    # print(f'directory={directory}\nadjusted_direcory={adjusted_direcory}\nlink_name={link_name}\n=> {result}')
+    adjusted_directory = strip_parent_directories_from_directory(directory)
+    if len(adjusted_directory) > 0:
+        adjusted_directory += '/'
+    result = f'-  [[{adjusted_directory}{link_name}|{link_name}]]\n'
+    # print(f'directory={directory}\nadjusted_directory={adjusted_directory}\nlink_name={link_name}\n=> {result}')
     return result
 
 
