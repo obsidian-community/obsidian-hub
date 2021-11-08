48bed13f578e6ac0559f0660b21144f50d5f70da
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-08 23:09:27 +0000

    Refactor to simplify process_directory()

--- .github/scripts/update-mocs.py
+++ .github/scripts/update-mocs.py
@@ -19,13 +19,12 @@ def process_all_directories(args):
 
 
 def process_directory(root, dirs, files):
-    moc_base_name = moc_base_name_for_directory(root)
     moc_file_path = moc_file_path_for_directory(root)
     new_moc_content_with_delimiters = make_moc_for_directory_with_delimiters(root, dirs, files)
     if os.path.exists(moc_file_path):
         rewrite_existing_moc_file(moc_file_path, new_moc_content_with_delimiters)
     else:
-        write_new_moc_file(moc_base_name, moc_file_path, new_moc_content_with_delimiters)
+        write_new_moc_file(root, moc_file_path, new_moc_content_with_delimiters)
 
 
 def rewrite_existing_moc_file(moc_file_path, new_moc_content_with_delimiters):
@@ -35,8 +34,9 @@ def rewrite_existing_moc_file(moc_file_path, new_moc_content_with_delimiters):
         output.write(update_existing_moc(initial_content, new_moc_content_with_delimiters))
 
 
-def write_new_moc_file(moc_base_name, moc_file_path, new_moc_content_with_delimiters):
+def write_new_moc_file(root, moc_file_path, new_moc_content_with_delimiters):
     template = get_template("directory_moc")
+    moc_base_name = moc_base_name_for_directory(root)
     new_content = template.render(title=moc_base_name, list_of_files_and_dirs=new_moc_content_with_delimiters)
     with open(moc_file_path, 'w') as output:
         output.write(new_content)
