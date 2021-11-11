4c8d1d66f42a863508b28b75e974863d8ed2b820
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-11 13:23:02 +0000

    Add some TODOs to later improve tests.py

--- .github/scripts/tests.py
+++ .github/scripts/tests.py
@@ -3,6 +3,9 @@
 # This script runs all the tests.
 # And it can be run from inside PyCharm or other IDEs, allowing debugging
 
+# TODO Figure out how to add -s argument, meaning "show console output if test fails"
+# TODO Then update run_tests.sh to invoke this script
+
 import os, pathlib
 import pytest
 
