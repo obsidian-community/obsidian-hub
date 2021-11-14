646d3ddd84ebebcd68bf78d1088b553cb8682498
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-14 00:05:40 +0000

    Add a test of Jinja2 template with minimal content
    It should be possible to compare the approved file of
    this test with that of test_author_from_templater(),
    and see minimal differences

--- .github/scripts/tests/test_templates.py
+++ .github/scripts/tests/test_templates.py
@@ -32,6 +32,13 @@ def test_author_from_jinja():
     verify(new_content, options=approval_test_options())
 
 
+def test_author_from_jinja_minimal():
+    template = utils.get_template_from_directory(JINJA_TEMPLATES_DIR, "author.md.jinja")
+    new_content = template.render(user="test-user", author="Test User")
+
+    verify(new_content, options=approval_test_options())
+
+
 def test_author_from_templater():
     template = utils.get_template_from_directory(CONTRIB_TEMPLATES_DIR, 'T - Author.md')
     new_content = template.render(title="test-user")
