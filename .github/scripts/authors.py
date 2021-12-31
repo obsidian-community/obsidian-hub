a7efb2c08af3b2c12c61852c769a386d445209b1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-12-31 23:26:38 +0000

    Add the remaining author names that are currently manually edited
    See #150

--- .github/scripts/authors.py
+++ .github/scripts/authors.py
@@ -7,6 +7,9 @@ def adjust_author(extension):
     # extension is a dict - either a plugin or a theme
     author = extension['author']
     substitutions = {
+        "Andrew Brown & Tim Hor": "Tim Hor",
+        "bicarlsen": "Brian Carlsen", # remove when https://github.com/bicarlsen/obsidian_image_caption/pull/7 closed
+        "Chetachi": "Chetachi E.",
         "ryanjamurphy": "Ryan J. A. Murphy"
     }
     if author in substitutions:
