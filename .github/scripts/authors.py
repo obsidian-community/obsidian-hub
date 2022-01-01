88a8258d01ebe0fa164592d494459cf3e87f9579
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-01-01 16:49:25 +0000

    Add typehints and better parameter name in adjust_author()

--- .github/scripts/authors.py
+++ .github/scripts/authors.py
@@ -1,15 +1,18 @@
-def adjust_author(extension):
+from typing import Dict
+
+
+def adjust_author(plugin: Dict[str, str]) -> None:
     """
     
-    :param extension: 
+    :param plugin: 
     :return: 
     """
     # extension is a dict - either a plugin or a theme
-    author = extension['author']
+    author = plugin['author']
     substitutions = {
         "Andrew Brown & Tim Hor": "Tim Hor",
         "Chetachi": "Chetachi E.",
         "ryanjamurphy": "Ryan J. A. Murphy"
     }
     if author in substitutions:
-        extension.update(author=substitutions[author])
+        plugin.update(author=substitutions[author])
