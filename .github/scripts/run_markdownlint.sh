0b1efe3ee8d0e03241bbf4b6a33373bec871f1e5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-01-23 14:09:01 +0000

    Reduce repetition in script
    Co-Authored-By: pseudometa <73286100+chrisgrieser@users.noreply.github.com>

--- .github/scripts/run_markdownlint.sh
+++ .github/scripts/run_markdownlint.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/markdownlint --fix ../../00\ -\ Contribute\ to\ the\ Obsidian\ Hub
+cd ../..
+
+MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --fix"
+
+${MARKDOWN_COMMAND} 00\ -\ Contribute\ to\ the\ Obsidian\ Hub
+${MARKDOWN_COMMAND}  "04 - Guides, Workflows, & Courses"
