f293c1f66a39b0aa71247882d2976f25355c3aad
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-01-23 14:11:36 +0000

    Disable some fixes to aid reviewing
    Co-Authored-By: pseudometa <73286100+chrisgrieser@users.noreply.github.com>

--- .github/scripts/run_markdownlint.sh
+++ .github/scripts/run_markdownlint.sh
@@ -2,7 +2,8 @@
 
 cd ../..
 
-MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --fix"
+# --disable no-multiple-blanks no-trailing-spaces added to make review of other changes easier
+MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --disable no-multiple-blanks no-trailing-spaces --fix"
 
 ${MARKDOWN_COMMAND} 00\ -\ Contribute\ to\ the\ Obsidian\ Hub
 ${MARKDOWN_COMMAND}  "04 - Guides, Workflows, & Courses"
