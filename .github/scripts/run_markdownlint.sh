5de8b6620b318b081f708b0267e10631274ffb84
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-02-03 14:10:45 +0000

    Disable all remaining markdown lint warnings
    Co-Authored-By: pseudometa <73286100+chrisgrieser@users.noreply.github.com>

--- .github/scripts/run_markdownlint.sh
+++ .github/scripts/run_markdownlint.sh
@@ -4,6 +4,9 @@ cd ../..
 
 OUTPUT_FILE='.github/scripts/markdownlint_output.txt'
 # --disable no-multiple-blanks no-trailing-spaces added to make review of other changes easier
+# For discussion:
+#  blanks-around-headings
+#  no-trailing-punctuation - in headings
 MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint \
   --disable \
     no-multiple-blanks \
@@ -11,6 +14,16 @@ MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint \
     list-marker-space \
     blanks-around-fences \
     fenced-code-language \
+    heading-increment \
+    heading-style \
+    list-indent \
+    blanks-around-headings \
+    heading-start-left \
+    no-trailing-punctuation \
+    ol-prefix \
+    no-space-in-emphasis \
+    no-empty-links \
+    single-trailing-newline \
   -o ${OUTPUT_FILE}"
 
 ${MARKDOWN_COMMAND} \
