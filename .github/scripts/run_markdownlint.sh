ebd63fd6ac383658c838dca082e9dfb307a81707
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-02-03 13:46:04 +0000

    Get summary of number of rules triggered
    Co-Authored-By: pseudometa <73286100+chrisgrieser@users.noreply.github.com>

--- .github/scripts/run_markdownlint.sh
+++ .github/scripts/run_markdownlint.sh
@@ -2,9 +2,12 @@
 
 cd ../..
 
+OUTPUT_FILE='.github/scripts/markdownlint_output.txt'
 # --disable no-multiple-blanks no-trailing-spaces added to make review of other changes easier
-MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --disable no-multiple-blanks no-trailing-spaces -o .github/scripts/markdownlint_output.txt"
+MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --disable no-multiple-blanks no-trailing-spaces -o ${OUTPUT_FILE}"
 
 ${MARKDOWN_COMMAND} \
   "00 - Contribute to the Obsidian Hub" \
   "04 - Guides, Workflows, & Courses"
+
+sed 's/^.* MD/MD/' ${OUTPUT_FILE} | sort | uniq -c | sort -n
