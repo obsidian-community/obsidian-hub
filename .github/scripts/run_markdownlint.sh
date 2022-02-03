21a60ae06c9c508b864f0b7b97cdf1e5dd13acef
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-02-03 13:27:41 +0000

    Save markdownlint output to file, to track changes
    Co-Authored-By: pseudometa <73286100+chrisgrieser@users.noreply.github.com>

--- .github/scripts/run_markdownlint.sh
+++ .github/scripts/run_markdownlint.sh
@@ -3,7 +3,8 @@
 cd ../..
 
 # --disable no-multiple-blanks no-trailing-spaces added to make review of other changes easier
-MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --disable no-multiple-blanks no-trailing-spaces --fix"
+MARKDOWN_COMMAND=".github/scripts/node_modules/.bin/markdownlint --disable no-multiple-blanks no-trailing-spaces -o .github/scripts/markdownlint_output.txt"
 
-${MARKDOWN_COMMAND} 00\ -\ Contribute\ to\ the\ Obsidian\ Hub
-${MARKDOWN_COMMAND}  "04 - Guides, Workflows, & Courses"
+${MARKDOWN_COMMAND} \
+  "00 - Contribute to the Obsidian Hub" \
+  "04 - Guides, Workflows, & Courses"
