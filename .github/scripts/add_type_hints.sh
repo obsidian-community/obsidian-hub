99885a2b1ddef2124fce7a7d064e11cbe176dd6e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-02-03 08:52:06 +0000

    Add monkeytype wrapper script to add type hints (#284)

---
+++ .github/scripts/add_type_hints.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Make an edit to doc/Features.md, to cover more code...
+
+# https://instagram-engineering.com/let-your-code-type-hint-itself-introducing-open-source-monkeytype-a855c7284881
+rm -f monkeytype.sqlite3
+monkeytype run -m pytest tests/*.py
+#monkeytype run ./add_footer.py > /dev/null
+#monkeytype run ./check_contents.py
+#monkeytype run ./update_mocs.py
+#monkeytype run ./update_releases.py
+#monkeytype run ./update_releases.py --all
+#monkeytype run ./update_releases.py --update-download-counts
+
+for module in $(monkeytype list-modules) ; do
+  echo updating $module
+  monkeytype apply $module
+done
