2f18545a99de93291151831f7580439197553ca9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-02-04 17:51:28 +0000

    Tidy up add_type_hints.sh - only run the tests

--- .github/scripts/add_type_hints.sh
+++ .github/scripts/add_type_hints.sh
@@ -5,12 +5,6 @@
 # https://instagram-engineering.com/let-your-code-type-hint-itself-introducing-open-source-monkeytype-a855c7284881
 rm -f monkeytype.sqlite3
 monkeytype run -m pytest tests/*.py
-monkeytype run ./add_footer.py > /dev/null
-#monkeytype run ./check_contents.py
-monkeytype run ./update_mocs.py
-#monkeytype run ./update_releases.py
-#monkeytype run ./update_releases.py --all
-#monkeytype run ./update_releases.py --update-download-counts
 
 for module in $(monkeytype list-modules) ; do
   echo updating $module
