12293b78b731ab302f5cf5432d43fb3fcfa0ccaf
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-02-25 10:27:03 +0100

    move get_community_plugins() to bottom of file

--- .github/scripts/themes.py
+++ .github/scripts/themes.py
@@ -166,11 +166,6 @@ def get_theme_plugin_support(
     return None
 
 
-def get_community_plugins() -> ThemeList:
-    plugin_list = get_json_from_github(PLUGINS_JSON_FILE)
-    return plugin_list
-
-
 def get_theme_downloads() -> ThemeDownloads:
     # Example content:
     # {
@@ -323,3 +318,8 @@ def set_theme_download_count(template: Template, current_name: str, new_download
 def update_theme_download_count(template: Template, theme_downloads: ThemeDownloads, current_name: str, verbose: bool) -> None:
     download_count = get_theme_current_download_count(theme_downloads, current_name)
     set_theme_download_count(template, current_name, download_count, verbose)
+
+
+def get_community_plugins() -> ThemeList:
+    plugin_list = get_json_from_github(PLUGINS_JSON_FILE)
+    return plugin_list
