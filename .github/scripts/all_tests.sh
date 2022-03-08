baa8beda6903580ec01afc7bd41b6fb4282bfa31
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-03-08 22:32:39 +0100

    check for directory and virtual environment

--- .github/scripts/all_tests.sh
+++ .github/scripts/all_tests.sh
@@ -3,6 +3,35 @@
 # exit if error
 set -e
 
+
+# check that in scripts directory
+
+CURRENT_DIR="$(pwd)"
+SCRIPTS_DIR='obsidian-hub/.github/scripts'
+
+if ! [[ "$CURRENT_DIR" =~ "$SCRIPTS_DIR" ]] ; then
+    echo "Your current directory is $CURRENT_DIR"
+    echo "It needs to be ...$SCRIPTS_DIR"
+    exit 1
+fi
+
+
+# check if virtual environment is activated
+
+PYTHON="$(which python)"
+PARTIAL_PATH='venv/bin/python'
+
+if ! [[ "$PYTHON" =~ "$PARTIAL_PATH" ]] ; then
+    echo 'The virtual environment is not active. Type the following to activate it:'
+    # Sourcing only works in interactive shells
+    # This is why it cannot be done by script
+    echo 'source ./../../venv/bin/activate'
+    exit 1
+fi
+
+
+# tests
+
 ./check_type_hints.sh
 
 cd ./tests
