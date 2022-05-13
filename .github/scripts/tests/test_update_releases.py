acd4eed1f9eacadd4c287cc7e40ee0e4e4344efb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-13 18:18:59 +0100

    Add testing helper get_processed_saved_sample_data_for_plugin()

--- .github/scripts/tests/test_update_releases.py
+++ .github/scripts/tests/test_update_releases.py
@@ -2,7 +2,7 @@ from approvaltests import verify_as_json
 
 from update_releases import collate_authors
 from tests.helpers_for_testing import get_saved_sample_data_for_theme, \
-    get_saved_sample_data_for_plugin
+    get_processed_saved_sample_data_for_plugin
 from utils import FileGroups
 
 
@@ -12,9 +12,7 @@ def test_process_authors() -> None:
     theme.collect_data_for_theme_and_css(css_file, theme_downloads, theme_file_groups)
     themes = [theme]
 
-    plugin, manifest = get_saved_sample_data_for_plugin("nldates-obsidian")
-    plugin_file_groups: FileGroups = dict()
-    plugin.collect_data_for_plugin_and_manifest(manifest, plugin_file_groups)
+    plugin = get_processed_saved_sample_data_for_plugin("nldates-obsidian")
     plugins = [plugin]
 
     all_authors = collate_authors(themes, plugins)
