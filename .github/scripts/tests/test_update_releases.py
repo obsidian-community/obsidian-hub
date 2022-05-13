2a937ad13566df0296320911e46f7c125bea42cb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-13 18:22:31 +0100

    Add testing helper get_processed_saved_sample_data_for_theme()

--- .github/scripts/tests/test_update_releases.py
+++ .github/scripts/tests/test_update_releases.py
@@ -1,15 +1,12 @@
 from approvaltests import verify_as_json
 
 from update_releases import collate_authors
-from tests.helpers_for_testing import get_saved_sample_data_for_theme, \
-    get_processed_saved_sample_data_for_plugin
-from utils import FileGroups
+from tests.helpers_for_testing import \
+    get_processed_saved_sample_data_for_plugin, get_processed_saved_sample_data_for_theme
 
 
 def test_process_authors() -> None:
-    theme, css_file, theme_downloads = get_saved_sample_data_for_theme("Christmas")
-    theme_file_groups: FileGroups = dict()
-    theme.collect_data_for_theme_and_css(css_file, theme_downloads, theme_file_groups)
+    theme = get_processed_saved_sample_data_for_theme("Christmas")
     themes = [theme]
 
     plugin = get_processed_saved_sample_data_for_plugin("nldates-obsidian")
